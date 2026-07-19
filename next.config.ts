de642bd689454d3c70e3405c4bc143f2b4377bf0
gh-phduy <evnnpd@gmail.com>
2026-07-19 23:02:36 +0700

    chore: initial project setup with tailwind and next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Cho phép tất cả các domain (phù hợp với Supabase URL động)
+      },
+    ],
+  },
 };
 
 export default nextConfig;
